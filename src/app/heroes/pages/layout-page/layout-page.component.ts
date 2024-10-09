1281e2002980c65d0dec602e43c46f5604dc1799
fran <l.francisco.bj@gmail.com>
2024-10-09 16:51:05 -0600

    Añadiendo opciones para los botones de navegacion del apartado de heroes

--- src/app/heroes/pages/layout-page/layout-page.component.ts
+++ src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styles: ``
 })
 export class LayoutPageComponent {
-
+  public sidebarItems = [
+    { label: 'List Hero', icon: 'list', url: './list' },
+    { label: 'Add Hero', icon: 'add', url: './newHero' },
+    { label: 'Searach Hero', icon: 'search', url: './search'}
+  ]
 }
