18b027541e8c5b8013276a5eedf214287c901b29
fran <l.francisco.bj@gmail.com>
2024-10-15 20:07:05 -0600

    Implementando servicios para la busqueda de heroes

--- src/app/heroes/pages/search-page/search-page.component.ts
+++ src/app/heroes/pages/search-page/search-page.component.ts
@@ -1,4 +1,8 @@
+import { Hero } from './../../interfaces/hero.interface';
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { HeroesService } from '../../services/hero.service';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 
 @Component({
   selector: 'app-search-page',
@@ -6,5 +10,27 @@ import { Component } from '@angular/core';
   styles: ``
 })
 export class SearchPageComponent {
+
+  public searchHeroInput = new FormControl('');
+  public heroes: Hero[] = [];
+  public selectedHero?: Hero;
+
+  constructor(private heroesService:HeroesService){}
+  searchHero() {
+    const value: string = this.searchHeroInput.value || '';
+
+    this.heroesService.getSuggestions(value)
+      .subscribe(heroes => this.heroes = heroes);
+  }
+
+  onSelectedOption(event: MatAutocompleteSelectedEvent): void{
+    const selectedHero: Hero = event.option.value as Hero;
+    if (!selectedHero) {
+      this.selectedHero = undefined;
+      return;
+    }
+    this.searchHeroInput.setValue(selectedHero.superhero || '');
+    this.selectedHero = selectedHero;
+  }
 
 }
